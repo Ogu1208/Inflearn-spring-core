c629a7a2066c11b33943c525b70cd3c0fcd5ded8
Ogu1208 <kdasunb6@gmail.com>
2024-09-12 12:45:55 +0900

    회원, 주문 도메인 구현, 새로운 할인 정책 개발

---
+++ src/main/java/com/example/springcore/OrderApp.java
@@ -0,0 +1,26 @@
+package com.example.springcore;
+
+import com.example.springcore.member.Grade;
+import com.example.springcore.member.Member;
+import com.example.springcore.member.MemberService;
+import com.example.springcore.member.MemberServiceImpl;
+import com.example.springcore.order.Order;
+import com.example.springcore.order.OrderService;
+import com.example.springcore.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(1L,"MemberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order.toString());
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
+    }
+}
---
+++ src/main/java/com/example/springcore/order/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.springcore.order;
+
+import com.example.springcore.discount.DiscountPolicy;
+import com.example.springcore.member.Member;
+import com.example.springcore.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
