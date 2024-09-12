7bd0096e6537c31959c19c36566fe365eb59cf97
Ogu1208 <kdasunb6@gmail.com>
2024-09-12 15:11:41 +0900

    feat: AppConfig 관심사 분리, 테스트 코드 수정

--- src/main/java/com/example/springcore/OrderApp.java
+++ src/main/java/com/example/springcore/OrderApp.java
@@ -3,16 +3,14 @@ package com.example.springcore;
 import com.example.springcore.member.Grade;
 import com.example.springcore.member.Member;
 import com.example.springcore.member.MemberService;
-import com.example.springcore.member.MemberServiceImpl;
 import com.example.springcore.order.Order;
 import com.example.springcore.order.OrderService;
-import com.example.springcore.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(1L,"MemberA", Grade.VIP);
@@ -20,7 +18,6 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
-        System.out.println("order = " + order.toString());
-        System.out.println("order.calculatePrice = " + order.calculatePrice());
+        System.out.println("order = " + order);
     }
 }
--- src/test/java/com/example/springcore/member/MemberServiceTest.java
+++ src/test/java/com/example/springcore/member/MemberServiceTest.java
@@ -1,11 +1,19 @@
 package com.example.springcore.member;
 
+import com.example.springcore.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach  // 각 테스트마다 실행
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void Join() {
