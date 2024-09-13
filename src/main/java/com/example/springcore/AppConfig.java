d5463bbaef459cb54354a1bd6eb1e6db6f00fd91
Ogu1208 <kdasunb6@gmail.com>
2024-09-13 10:55:01 +0900

    feat: 스프링 빈 등록, 모든 빈 조회, 동일타입 둘 이상 빈 조회 테스트코드 작성 (~ch5.4)

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -8,21 +8,29 @@ import com.example.springcore.member.MemberServiceImpl;
 import com.example.springcore.member.MemoryMemberRepository;
 import com.example.springcore.order.OrderService;
 import com.example.springcore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    // 메서드 이름으로 스프링 빈의 이름을 설정한다.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
