96aeaa03f3f7a4465e4e6b3ac83d0ae89cebb420
Ogu1208 <kdasunb6@gmail.com>
2025-01-04 19:41:40 +0900

    [Sec6-6] @Configuration과 싱글톤

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -14,20 +14,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // 1. call AppConfig.memberService (memberService() 호출)
+    // call AppConfig.memberRepository
+    // 2. call AppConfig.memberRepository (memberRepository() 호출)
+    // 3. call AppConfig.orderService
+    // call AppConfig.memberRepository
+    // --------- 실제 호출 디버깅 : 메서드가 한번씩만 호출됨
+    // 1. call AppConfig.memberService
+    // 2. call AppConfig.memberRepository
+    // 3. call AppConfig.orderService
+
     // 메서드 이름으로 스프링 빈의 이름을 설정한다.
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
